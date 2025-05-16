f1d8495fb0cbfc592af809f44903842fd2886bd0
Suciu <sergiosuciu@gmail.com>
2025-05-15 21:20:36 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const post = process.env.POST || 3001; //vai rodar na porta da variavel de ambientação ou na 3001
+const app = express();
+
+app.listen(post, () => {
+    console.log(`Servidor rodando na porta ${post}`);
+})
