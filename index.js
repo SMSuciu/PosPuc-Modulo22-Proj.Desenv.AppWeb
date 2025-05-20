40a8ceb2ea8e7b4636da298b2d93e49db64577d6
Suciu <sergiosuciu@gmail.com>
2025-05-20 19:43:04 -0300

    Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const post = process.env.POST || 3001; //vai rodar na porta da variavel de ambientação ou na 3001
 const app = express();
 
+app.get('/', (req, res) => {
+    //res.send('Hello World');
+    console.log('Nova Feacture');
+});
+
 app.listen(post, () => {
     console.log(`Servidor rodando na porta ${post}`);
 })
